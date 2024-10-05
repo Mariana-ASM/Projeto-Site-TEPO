a0d11d331eafce86d2d216810b4e96837a315023
Mariana Araújo de Souza Monteiro <88739032+Mariana-ASM@users.noreply.github.com>
2024-10-05 14:05:50 +0000

    Links para serem acessados

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("app").innerHTML = <h1></h1>;
+
+
+
+https://helpdesk.cin.ufpe.br/servicos
+
+https://www.ufpe.br/documents/40780/4018248/EDITAL+trans+externa+-+TE+2022+DESTAQUES+ALTERA%C3%87%C3%95ES.pdf/3a667f21-7801-4745-953f-2f5b29705271
+
+https://www.ufpe.br/formas-de-ingresso/transferencia
+
+https://awari.com.br/como-utilizar-a-metodologia-agile-canvas-para-impulsionar-seus-projetos/#:~:text=nos%20seus%20projetos%3A-,O%20que%20%C3%A9%20a%20metodologia%20Agile%20Canvas%3F,elementos%20essenciais%20de%20um%20projeto.
+
+https://awari.com.br/como-utilizar-a-metodologia-agile-canvas-para-impulsionar-seus-projetos-3/
